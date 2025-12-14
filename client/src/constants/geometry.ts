e325085df4e58a5b452134e3fc122ee3ba473102
e1isev <138175486+e1isev@users.noreply.github.com>
2025-12-15 09:16:08 +1100

    Increase snapping radius

--- client/src/constants/geometry.ts
+++ client/src/constants/geometry.ts
@@ -1,3 +1,3 @@
 export const MIN_LINE_LENGTH_MM = 300;
-export const SNAP_RADIUS_MM = 400;
-export const DEFAULT_SNAP_TOLERANCE = 40;
+export const SNAP_RADIUS_MM = 500;
+export const DEFAULT_SNAP_TOLERANCE = 50;
